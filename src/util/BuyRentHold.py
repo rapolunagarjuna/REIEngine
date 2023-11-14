af52707270ccc260dfe506ad4e60f9d21d7f2884
Nagarjuna Rapolu <112997527+rapolunagarjuna@users.noreply.github.com>
2023-11-14 01:07:42 -0500

    Edit Calculator Changes

--- src/util/BuyRentHold.py
+++ src/util/BuyRentHold.py
@@ -1,27 +1,23 @@
 class BuyRentHold:
-    def __init__(self, offer_price_USD, units, annual_rent_USD, insurance_USD, property_tax_USD):
-        # if offer_price_USD is None or units is None or annual_rent_USD is None:
-        #     raise Exception("Arguments are missing")
-        # print(type(offer_price_USD))
-        # if (not isinstance(offer_price_USD, float) )or (not isinstance(offer_price_USD, int)):
-        #     raise Exception("Offer price is not a float/ integer")
-        # if not isinstance(units, int):
-        #     raise Exception("Units are not an integer")
+    def __init__(self, offer_price_USD, units, insurance_USD, property_tax_USD, defaults):
         
-        self.property_info = PropertyInfo(offer_price_USD, units)
-        self.purchase_info = PurchaseInfo(offer_price_USD)
-        self.income_info = IncomeInfo(annual_rent_USD)
+
+        
+        self.property_info = PropertyInfo(offer_price_USD, units, defaults)
+        self.purchase_info = PurchaseInfo(offer_price_USD, defaults)
+        self.income_info = IncomeInfo(defaults["incomeInfo"]["grossRents"], defaults)
         self.operating_expenses = OperatingExpenses(
             property_tax_USD, 
             insurance_USD, 
-            annual_rent_USD, 
+            defaults["incomeInfo"]["grossRents"], 
             self.property_info.management_rate_percent,
             self.property_info.num_of_units, 
             self.property_info.advertising_cost_per_vacancy_USD, 
-            self.property_info.vacancy_rate_percent
+            self.property_info.vacancy_rate_percent,
+            defaults
             )
-        self.financing = Financing(offer_price_USD, self.purchase_info.real_purchase_price_USD)
-        self.cash_requirements = CashRequirements(self.financing.cash_required_to_close_USD)
+        self.financing = Financing(offer_price_USD, self.purchase_info.real_purchase_price_USD, defaults)
+        self.cash_requirements = CashRequirements(self.financing.cash_required_to_close_USD, defaults)
 
 
         #summary statistics
@@ -52,29 +48,28 @@ class BuyRentHold:
 
 
 class CashRequirements:
-    def __init__(self, cash_required_to_close_USD):
+    def __init__(self, cash_required_to_close_USD, defaults):
         self.deposits_made_with_offer_USD = 0	
         self.less_pro_ration_of_rents_USD = 0	
         self.cash_required_to_close_USD = cash_required_to_close_USD - self.deposits_made_with_offer_USD 
         self.total_cash_required_USD = cash_required_to_close_USD - self.less_pro_ration_of_rents_USD
-        
 
 class Financing:
-    def __init__(self, offer_price_USD, real_purchase_price_USD):
+    def __init__(self, offer_price_USD, real_purchase_price_USD, defaults):
         self.offer_price_USD = offer_price_USD
         self.real_purchase_price_USD = real_purchase_price_USD
 
 
-        self.first_mtg_principle_borrowed_USD = offer_price_USD*0.95 
-        self.first_mtg_interest_rate_percent = 8
-        self.first_mtg_amortization_period_years = 30
-        self.first_mtg_CHMC_fee_percent = 0
+        self.first_mtg_principle_borrowed_USD = defaults["financing"]["firstMtgPrincipleAmount"]
+        self.first_mtg_interest_rate_percent = defaults["financing"]["firstMtgInterestRate"]
+        self.first_mtg_amortization_period_years = defaults["financing"]["firstMtgAmortizationPeriod"]
+        self.first_mtg_CHMC_fee_percent = defaults["financing"]["firstMtgCMHCFee"]
         self.first_mtg_total_principle_USD = self.first_mtg_principle_borrowed_USD*(1 + (self.first_mtg_CHMC_fee_percent/100))
         self.first_mtg_monthly_payment_USD = self.first_mtg_total_principle_USD*((((1+(self.first_mtg_interest_rate_percent/200))**(1/6))-1)/(1-(((1+(self.first_mtg_interest_rate_percent/200))**(1/6))**(self.first_mtg_amortization_period_years*-12))))
 
-        self.second_mtg_principle_borrowed_USD = 0
-        self.second_mtg_interest_rate_percent = 12
-        self.second_mtg_amortization_period_years = 99999
+        self.second_mtg_principle_borrowed_USD = defaults["financing"]["secondMtgPrincipleAmount"]
+        self.second_mtg_interest_rate_percent = defaults["financing"]["secondMtgInterestRate"]
+        self.second_mtg_amortization_period_years = defaults["financing"]["secondMtgAmortizationPeriod"]
         self.second_mtg_monthly_payment_USD = self.second_mtg_principle_borrowed_USD*((((1+(self.second_mtg_interest_rate_percent/200))**(1/6))-1)/(1-(((1+(self.second_mtg_interest_rate_percent/200))**(1/6))**(self.second_mtg_amortization_period_years*-12))))
 
         self.interest_only_principle_amount_USD = 0
@@ -84,14 +79,11 @@ class Financing:
         self.other_monthly_financing_costs_USD = 350
         self.cash_required_to_close_USD = self.real_purchase_price_USD - self.first_mtg_principle_borrowed_USD - self.second_mtg_principle_borrowed_USD - self.interest_only_principle_amount_USD
 
-
-
-
-
 class OperatingExpenses:
-    def __init__(self, property_tax_USD, insurance_USD, annual_rent_USD, management_rate_percent, units, advertising_cost_per_vacancy_USD, vacancy_rate_percent,  repairs_percent = 5):
+    def __init__(self, property_tax_USD, insurance_USD, annual_rent_USD, management_rate_percent, units, advertising_cost_per_vacancy_USD, vacancy_rate_percent, defaults):
         #storing arguments
-        self.repairs_percent = repairs_percent
+
+        self.repairs_percent = defaults["operating"]["repairsRate"]
         self.management_rate_percent = management_rate_percent
         self.annual_rent_USD = annual_rent_USD
         self.units = units
@@ -102,26 +94,26 @@ class OperatingExpenses:
         self.property_tax_USD = property_tax_USD
         self.insurance_USD =  insurance_USD
         self.repairs_USD = self.repairs_percent * annual_rent_USD / 100
-        self.electricity_USD = 0   
-        self.gas_USD = 0
-        self.lawn_snow_maintenance_USD = 0
-        self.water_sewer_USD = 1200
-        self.cable_USD = 1200
+        self.electricity_USD = defaults["operating"]["electricity"]
+        self.gas_USD = defaults["operating"]["gas"]
+        self.lawn_snow_maintenance_USD = defaults["operating"]["lawn"]
+        self.water_sewer_USD = defaults["operating"]["water"]
+        self.cable_USD = defaults["operating"]["cable"]
         self.management_USD = self.management_rate_percent * self.annual_rent_USD / 100
-        self.caretaking_USD = 2880
+        self.caretaking_USD = defaults["operating"]["caretaking"]
         self.advertizing_USD = 12 * self.units * self.vacancy_rate_percent * self.advertising_cost_per_vacancy_USD / 200  
-        self.association_fees_USD = 0
+        self.association_fees_USD = defaults["operating"]["associationFees"]
         self.pest_control_USD = self.units*140 if self.units < 2 else self.units * 70
-        self.security_USD = (self.vacancy_rate_percent/ 100) * self.units * 12 * 50 / 1.5
-        self.trash_removal_USD = 0
-        self.misc_USD = 0
-        self.common_area_maintenance_USD = 0   
-        self.capital_improvements_USD = 0   
-        self.accounting_USD = 0		
-        self.legal_USD = 0		
-        self.bad_debts_USD = 0
-        self.other_USD = 0	
-        self.evictions_USD = 180
+        self.security_USD = (self.vacancy_rate_percent/ 100) * self.units * 12 /1.5 * 50
+        self.trash_removal_USD = defaults["operating"]["trashRemoval"]
+        self.misc_USD = defaults["operating"]["miscellaneous"]
+        self.common_area_maintenance_USD = defaults["operating"]["commonAreaMaintenance"] 
+        self.capital_improvements_USD = defaults["operating"]["capitalImprovements"] 
+        self.accounting_USD = defaults["operating"]["accounting"] 
+        self.legal_USD = defaults["operating"]["legal"]
+        self.bad_debts_USD = defaults["operating"]["badDebts"] 
+        self.other_USD = defaults["operating"]["other"] 
+        self.evictions_USD = (((units * 12 * vacancy_rate_percent/100)/2)/10)*1000
 
         self.total_expenses_USD = (
             self.property_tax_USD +
@@ -149,34 +141,29 @@ class OperatingExpenses:
             self.evictions_USD
         )
 
-
-
-
-
-
 class PropertyInfo:
-    def __init__(self, offer_price_USD, units, vacancy_rate_percent = 5, management_rate_percent = 5, advertising_cost_per_vacancy_USD = 100, annual_appreciation_rate_percent = 3):
+    def __init__(self, offer_price_USD, units, defaults):
         self.fair_market_value_USD = offer_price_USD
-        self.vacancy_rate_percent = vacancy_rate_percent
-        self.management_rate_percent = management_rate_percent
-        self.advertising_cost_per_vacancy_USD = advertising_cost_per_vacancy_USD
+        self.vacancy_rate_percent = defaults["propertyInfo"]["vacancyRate"]
+        self.management_rate_percent = defaults["propertyInfo"]["managementRate"]
+        self.advertising_cost_per_vacancy_USD = defaults["propertyInfo"]["advertizingCost"]
         self.num_of_units = units
-        self.annual_appreciation_rate_percent = annual_appreciation_rate_percent
-
+        self.annual_appreciation_rate_percent = defaults["propertyInfo"]["annualAppreciationRate"]
 
 class PurchaseInfo:
-    def __init__(self, offer_price_USD, repairs_USD = 5000, repairs_contingency_USD = 1000, lender_fee_USD = 1500, broker_fee_USD = -5000, environmentals_USD = 0, inspections_USD = 700, appraisals_USD = 700, misc_USD = 500, transfer_tax_USD = 0, legal_USD = 500):
+    def __init__(self, offer_price_USD, defaults):
+
         self.offer_price_USD = offer_price_USD
-        self.repairs_USD = repairs_USD
-        self.repairs_contingency_USD = repairs_contingency_USD
-        self.lender_fee_USD = lender_fee_USD
-        self.broker_fee_USD = broker_fee_USD
-        self.environmentals_USD = environmentals_USD
-        self.inspections_USD = inspections_USD
-        self.appraisals_USD = appraisals_USD
-        self.misc_USD = misc_USD
-        self.transfer_tax_USD = transfer_tax_USD
-        self.legal_USD = legal_USD
+        self.repairs_USD = defaults["purchaseInfo"]["repairs"]
+        self.repairs_contingency_USD = defaults["purchaseInfo"]["repairsContingency"]
+        self.lender_fee_USD = defaults["purchaseInfo"]["lenderFee"]
+        self.broker_fee_USD = -1 * defaults["purchaseInfo"]["brokerFee"]
+        self.environmentals_USD = defaults["purchaseInfo"]["environmentals"]
+        self.inspections_USD = defaults["purchaseInfo"]["inspections"]
+        self.appraisals_USD = defaults["purchaseInfo"]["appraisals"]
+        self.misc_USD = defaults["purchaseInfo"]["misc"]
+        self.transfer_tax_USD = defaults["purchaseInfo"]["transferTax"]
+        self.legal_USD = defaults["purchaseInfo"]["legal"]
 
         self.real_purchase_price_USD = (
             self.offer_price_USD + 
@@ -193,13 +180,13 @@ class PurchaseInfo:
         )
 
 class IncomeInfo:
-    def __init__(self, annual_rent_USD, parking_USD = 0, storage_USD = 0, laundry_USD = 0, other_USD = 0):
+    def __init__(self, annual_rent_USD, defaults):
 
-        self.annual_rent_USD = annual_rent_USD
-        self.parking_USD = parking_USD
-        self.storage_USD = storage_USD
-        self.laundry_USD = laundry_USD
-        self.other_USD = other_USD
+        self.annual_rent_USD = defaults["incomeInfo"]["grossRents"]
+        self.parking_USD = defaults["incomeInfo"]["parking"]
+        self.storage_USD = defaults["incomeInfo"]["storage"]
+        self.laundry_USD = defaults["incomeInfo"]["laundry"]
+        self.other_USD = defaults["incomeInfo"]["other"]
         self.total_income_USD = (
             self.annual_rent_USD + self.parking_USD + self.storage_USD
             + self.laundry_USD + self.other_USD
